c762bd42309f97cbdfa598e35720d719df3b2ff8
sypark <upperrr@gmail.com>
2024-06-07 13:57:42 +0900

    :sparkles: feat: login 기능 추가

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="form-signin w-100 m-auto">
-    <form>
       <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
       <div class="form-floating">
-        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" v-model="state.form.email">
         <label for="floatingInput">Email address</label>
       </div>
       <div class="form-floating">
-        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password" v-model="state.form.password">
         <label for="floatingPassword">Password</label>
       </div>
 
@@ -18,14 +17,33 @@
           Remember me
         </label>
       </div>
-      <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+      <button class="btn btn-primary w-100 py-2" @click="submit()">Sign in</button>
       <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2024</p>
-    </form>
   </div>
 </template>
 
 <script>
-export default {}
+import {reactive} from "vue";
+import axios from "axios";
+
+export default {
+  setup() {
+    const state = reactive({
+      form: {
+        email: "",
+        password: "",
+      }
+    })
+    const submit = () => {
+      axios.post("/api/account/login", state.form).then((res) => {
+        console.log(res);
+        window.alert("로그인되었습니다");
+      })
+    }
+
+    return { state, submit }
+  }
+}
 </script>
 
 <style scoped>
